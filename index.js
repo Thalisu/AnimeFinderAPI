fc454121c9388087458d6ba9c7761c5b0a47e783
Thalisu <thalisondmcc@gmail.com>
2024-05-08 17:31:27 -0300

    heroku deploy

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on: http://localhost:${PORT}`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on: ${PORT}`);
 });
